86b295a27a6880642bc6035c96cd2451cd1c012b
AnastaciaVolkova <polarbolide@gmail.com>
2020-06-08 02:17:41 +0200

    fix: Fix AddEdgeToChildNode parameters

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -12,7 +12,7 @@ void GraphNode::AddEdgeToParentNode(GraphEdge *edge) {
   _parentEdges.push_back(edge);
 }
 
-void GraphNode::AddEdgeToChildNode(unique_ptr<GraphEdge> &&edge) {
+void GraphNode::AddEdgeToChildNode(unique_ptr<GraphEdge> edge) {
   _childEdges.push_back(move(edge));
 }
 
